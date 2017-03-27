39267a64d90cfdaa6a0849844f98abd353fa421b
Danny Maher <dmaher724@gmail.com>
2017-03-27 16:09:52 -0500

    Unlock additional tests, adjust compute method to make tests pass for various DNA strands

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,9 +1,19 @@
 class Hamming
 
   def self.compute(strand_1 = "", strand_2 = "")
-    if strand_1 == strand_2
-      0
+    hamming_distance = 0
+
+    return hamming_distance if strand_1 == strand_2
+
+    if strand_1.length == strand_2.length
+      for i in 0...strand_1.length
+        if strand_1[i] != strand_2[i]
+          hamming_distance +=1
+        end
+      end
     end
+
+    hamming_distance
   end
 
 end
