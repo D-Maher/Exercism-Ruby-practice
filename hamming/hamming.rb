c084a71e7c2b3f54fa1cbab38c81481b8493ffcc
Danny Maher <dmaher724@gmail.com>
2017-03-27 16:13:48 -0500

    Adjust compute method to account for DNA strings of unequal length

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -5,14 +5,14 @@ class Hamming
 
     return hamming_distance if strand_1 == strand_2
 
-    if strand_1.length == strand_2.length
-      for i in 0...strand_1.length
-        if strand_1[i] != strand_2[i]
-          hamming_distance +=1
-        end
+    raise ArgumentError if strand_1.length != strand_2.length
+
+    for i in 0...strand_1.length
+      if strand_1[i] != strand_2[i]
+        hamming_distance +=1
       end
     end
-
+    
     hamming_distance
   end
 
