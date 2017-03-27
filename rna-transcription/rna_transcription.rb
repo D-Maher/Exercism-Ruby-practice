f50d3a439e357ccd66e13dbae892ca3ae591c03b
Danny Maher <dmaher724@gmail.com>
2017-03-27 18:32:23 -0500

    Add of_dna method to compute RNA complement of DNA input string for valid inputs

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,5 +1,27 @@
 class Complement
 
-  
+  def self.of_dna(dna_strand)
+    rna_strand = ""
+
+    dna_strand.chars.each do |nuc|
+      case nuc
+
+      when "G"
+        rna_strand += "C" 
+
+      when "C"
+        rna_strand += "G" 
+
+      when "T"
+        rna_strand += "A" 
+
+      when "A"
+        rna_strand += "U" 
+
+      end
+    end
+
+    rna_strand
+  end
 
 end
