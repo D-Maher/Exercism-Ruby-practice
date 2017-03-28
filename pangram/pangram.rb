e8dcad3eb97f7fc0e712d45dea2888ee677061e6
Danny Maher <dmaher724@gmail.com>
2017-03-28 13:35:26 -0500

    Add constant ALPHABET to compare phrases against, add pangram? method to determine if input phrase is anagram, introduce all tests

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,4 +1,16 @@
+class Pangram
 
+  ALPHABET = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+
+  def self.pangram?(phrase)
+    return false if phrase.empty?
+
+    letters = phrase.downcase.chars.select { |char| ALPHABET.include?(char) }.uniq.sort
+
+    letters == ALPHABET
+  end
+
+end
 
 
 module BookKeeping
