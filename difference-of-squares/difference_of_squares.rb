76654022ae45042577dc8963160ab17e91d84b8d
Danny Maher <dmaher724@gmail.com>
2017-03-28 12:51:36 -0500

    Declare Squares class, add initialize method to store number of natural numbers, add square_of_sum method to calculate the square of the sum of the first N natural numbers

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -1,4 +1,22 @@
+class Squares
+  def initialize(n)
+    @max_natural = n
+  end
 
+  def square_of_sum
+    n = max_natural
+
+    sum = (1..max_natural).reduce(0, :+)
+
+    square_of_sum = sum ** 2
+  end
+
+
+  private
+
+  attr_reader :max_natural
+
+end
 
 
 module BookKeeping
