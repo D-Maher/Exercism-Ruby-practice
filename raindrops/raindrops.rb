6c39d0fa5cf744199c71848fde2e6b7aa5cbf158
Danny Maher <dmaher724@gmail.com>
2017-03-28 12:29:09 -0500

    Declared class Raindrops, add method convert to convert integers to raindrop speak based on their factors

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,4 +1,26 @@
+class Raindrops
 
+  def self.convert(num)
+    raindrop_speak = ""
+
+    if num % 3 == 0
+      raindrop_speak += "Pling"
+    end
+
+    if num % 5 == 0
+      raindrop_speak += "Plang"
+    end
+
+    if num % 7 == 0
+      raindrop_speak += "Plong"
+    end
+
+    return num.to_s if raindrop_speak.empty?
+
+    raindrop_speak
+  end
+
+end
 
 
 module BookKeeping
