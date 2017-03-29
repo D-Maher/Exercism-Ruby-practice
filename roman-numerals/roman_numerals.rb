e97cbf1c07c49d56de10dd0721cb3d30113d9212
Danny Maher <dmaher724@gmail.com>
2017-03-29 12:43:06 -0500

    Unlock tests, add method to_roman to class Fixnum to convert numbers to Roman numerals

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,4 +1,38 @@
+class Fixnum
 
+  def to_roman
+    num = self
+    roman_string = ""
+
+    until num == 0
+      if num >= 1000
+        roman_string += "M"
+        num -= 1000
+      elsif num >= 500
+        roman_string += "D"
+        num -= 500
+      elsif num >= 100
+        roman_string += "C"
+        num -= 100
+      elsif num >= 50
+        roman_string += "L"
+        num -= 50
+      elsif num >= 10
+        roman_string += "X"
+        num -= 10
+      elsif num >= 5
+        roman_string += "V"
+        num -= 5
+      elsif num >= 1
+          roman_string += "I"
+        num -= 1
+      end
+    end
+
+    roman_string
+  end
+
+end
 
 
 module BookKeeping
