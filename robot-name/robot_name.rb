097f58755911bebf7b536390753726a623d8e285
Danny Maher <dmaher724@gmail.com>
2017-03-30 12:04:27 -0500

    Unlock name tests, add method generate_name to generate random robot name

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -1,4 +1,33 @@
+UPPERCASE = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
 
+DIGITS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+NAMES_IN_USE = []
+
+
+class Robot
+
+  attr_reader :name
+
+  def initialize
+    @name = generate_name
+  end
+
+  def generate_name
+    name = ""
+
+    until !NAMES_IN_USE.include?(name) && !name.empty?
+      2.times { name += UPPERCASE.sample }
+
+      3.times { name += DIGITS.sample.to_s }
+    end
+
+    NAMES_IN_USE << name
+
+    name
+  end
+
+end
 
 
 module BookKeeping
