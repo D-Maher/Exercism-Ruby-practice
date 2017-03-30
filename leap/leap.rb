0b28cc4e1524c95f2b15eaa49fcbd59ca878a400
Danny Maher <dmaher724@gmail.com>
2017-03-30 15:36:28 -0500

    Unlock all tests, declare class Year, add class method leap? to determine if a give year is a leap year

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,4 +1,24 @@
+class Year
 
+  def self.leap?(year)
+    is_leap_year = false
+
+    if year % 4 == 0
+      is_leap_year = true
+
+      if year % 100 == 0
+        is_leap_year = false
+      end
+
+      if year % 400 == 0
+        is_leap_year = true
+      end
+    end
+
+    is_leap_year
+  end
+
+end
 
 
 module BookKeeping
