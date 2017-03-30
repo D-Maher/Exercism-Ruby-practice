bd4fc08df75bcbb831f5d024564f1808d7c788a9
Danny Maher <dmaher724@gmail.com>
2017-03-30 12:35:54 -0500

    Add method is_prime? to check if an integer is prime?

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,15 +1,34 @@
 class Prime
 
-  def self.nth(number)
-    raise ArgumentError if number == 0
+  def self.nth(n)
+    raise ArgumentError if n == 0
 
     primes_found = 0
 
+    until primes_found == n
 
 
+
+
+    end
 
   end
 
+end
+
+
+def is_prime?(number)
+  return false if number.even? && number != 2
+  return false if number < 2
+
+  check_divisor = number - 1
+
+  while check_divisor > 1
+    return false if number % check_divisor == 0
+    check_divisor -= 1
+  end
+
+  true
 end
 
 
