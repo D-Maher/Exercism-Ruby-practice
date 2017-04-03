730ecde1e3bed4e080bf6f5c8e9da7f03282f84c
Danny Maher <dmaher724@gmail.com>
2017-04-03 14:37:27 -0500

    Unlock remaining test to ensure that encode and decode methods can handle unicode

--- run-length-encoding/run_length_encoding_test.rb
+++ run-length-encoding/run_length_encoding_test.rb
@@ -40,14 +40,12 @@ class RunLengthEncodingTest < Minitest::Test
   end
 
   def test_encode_unicode
-    skip
     input = '⏰⚽⚽⚽⭐⭐⏰'
     output = '⏰3⚽2⭐⏰'
     assert_equal output, RunLengthEncoding.encode(input)
   end
 
   def test_decode_unicode
-    skip
     input = '⏰3⚽2⭐⏰'
     output = '⏰⚽⚽⚽⭐⭐⏰'
     assert_equal output, RunLengthEncoding.decode(input)
