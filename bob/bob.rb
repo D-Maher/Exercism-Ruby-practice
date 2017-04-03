2c279e90a4c6f535ad5beec699d865da86c47669
Danny Maher <dmaher724@gmail.com>
2017-04-03 13:12:02 -0500

    Add class method hey that allows Bob to respond to questions and shouting, Bob defaults to saying 'Whatever.'

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,4 +1,17 @@
 class Bob
 
+  def self.hey(remark)
+    response = "Whatever."
+
+    if remark.include?("?")
+      response = "Sure."
+    end
+
+    if remark.count("a-zA-Z") > 0 && remark.upcase == remark
+      response = "Whoa, chill out!"
+    end
+
+    response
+  end
 
 end
--- bob/bob_test.rb
+++ bob/bob_test.rb
@@ -18,79 +18,66 @@ class BobTest < Minitest::Test
   end
 
   def test_shouting
-    skip
     remark = 'WATCH OUT!'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
 
   def test_shouting_gibberish
-    skip
     remark = ('A'..'Z').to_a.sample(10).join
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
 
   def test_asking_a_question
-    skip
     remark = 'Does this cryogenic chamber make me look fat?'
     assert_equal 'Sure.', bob.hey(remark), feedback(remark)
   end
 
   def test_asking_a_numeric_question
-    skip
     remark = 'You are, what, like 15?'
     assert_equal 'Sure.', bob.hey(remark), feedback(remark)
   end
 
   def test_asking_gibberish
-    skip
     remark = ('a'..'z').to_a.sample(10).join << '?'
     assert_equal 'Sure.', bob.hey(remark), feedback(remark)
   end
 
   def test_talking_forcefully
-    skip
     remark = "Let's go make out behind the gym!"
     assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
   end
 
   def test_using_acronyms_in_regular_speech
-    skip
     remark = "It's OK if you don't want to go to the DMV."
     assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
   end
 
   def test_forceful_questions
-    skip
     remark = 'WHAT THE HELL WERE YOU THINKING?'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
 
   def test_shouting_numbers
-    skip
     remark = '1, 2, 3 GO!'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
 
   def test_only_numbers
-    skip
     remark = '1, 2, 3'
     assert_equal 'Whatever.', bob.hey(remark), feedback(remark)
   end
 
   def test_question_with_only_numbers
-    skip
     remark = '4?'
     assert_equal 'Sure.', bob.hey(remark), feedback(remark)
   end
 
   def test_shouting_with_special_characters
-    skip
     remark = 'ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
 
   def test_shouting_with_no_exclamation_mark
-    skip
     remark = 'I HATE YOU'
     assert_equal 'Whoa, chill out!', bob.hey(remark), feedback(remark)
   end
