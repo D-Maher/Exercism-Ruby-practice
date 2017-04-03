33166df9707339560e531fca30be79acf91cb61d
Danny Maher <dmaher724@gmail.com>
2017-04-03 14:12:35 -0500

    Add class method decode to decode run length encoded inputs

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -3,17 +3,36 @@ class RunLengthEncoding
   def self.encode(input)
     encoded_input = ""
 
-    groups = input.scan(/((\w)\2*)/)
+    runs = input.scan(/((.)\2*)/)
 
-    groups.each do |group|
-      if group[0].length > 1
-        encoded_input += group[0].length.to_s 
+    runs.each do |run|
+      if run[0].length > 1
+        encoded_input += run[0].length.to_s 
       end
 
-      encoded_input += group[1]
+      encoded_input += run[1]
     end
 
     encoded_input
+  end
+
+  def self.decode(input)
+    decoded_input = ""
+
+    runs = input.scan(/(\d\w|
+      \w)/).flatten
+
+    runs.each do |run|
+      run_info = run.chars[0]
+
+      if run_info.to_i > 0
+        run_info.to_i.times { decoded_input += run.chars[1] }
+      else
+        decoded_input += run_info
+      end 
+    end
+
+    decoded_input
   end
 
 end
