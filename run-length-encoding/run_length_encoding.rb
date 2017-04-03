75b0c5ab2ff8d2a068e84520a2404fb2d3256891
Danny Maher <dmaher724@gmail.com>
2017-04-03 13:58:35 -0500

    Add class method encode to use run length encoding on an input string comprised of letters

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -1,5 +1,20 @@
 class RunLengthEncoding
 
+  def self.encode(input)
+    encoded_input = ""
+
+    groups = input.scan(/((\w)\2*)/)
+
+    groups.each do |group|
+      if group[0].length > 1
+        encoded_input += group[0].length.to_s 
+      end
+
+      encoded_input += group[1]
+    end
+
+    encoded_input
+  end
 
 end
 
