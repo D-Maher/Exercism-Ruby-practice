429d7b2b1c40ec476edf7ac24f649d00b35095c5
Danny Maher <dmaher724@gmail.com>
2017-04-04 13:23:43 -0500

    Declare class Binary, add class method to_decimal to covert valid binary strings into decimal numbers

--- binary/binary.rb
+++ binary/binary.rb
@@ -1,4 +1,22 @@
+class Binary
 
+  def self.to_decimal(bin_string)
+
+    split_bin = bin_string.chars.map { |char| char.to_i }
+
+    decimal = 0
+    counter = split_bin.length - 1
+
+    split_bin.each do |num|
+      decimal += num * (2 ** counter)
+      counter -= 1
+    end
+
+    decimal
+
+  end
+
+end
 
 
 module BookKeeping
--- binary/binary_test.rb
+++ binary/binary_test.rb
@@ -8,47 +8,38 @@ require_relative 'binary'
 # 82eb00d
 class BinaryTest < Minitest::Test
   def test_binary_0_is_decimal_0
-    # skip
     assert_equal 0, Binary.to_decimal('0')
   end
 
   def test_binary_1_is_decimal_1
-    skip
     assert_equal 1, Binary.to_decimal('1')
   end
 
   def test_binary_10_is_decimal_2
-    skip
     assert_equal 2, Binary.to_decimal('10')
   end
 
   def test_binary_11_is_decimal_3
-    skip
     assert_equal 3, Binary.to_decimal('11')
   end
 
   def test_binary_100_is_decimal_4
-    skip
     assert_equal 4, Binary.to_decimal('100')
   end
 
   def test_binary_1001_is_decimal_9
-    skip
     assert_equal 9, Binary.to_decimal('1001')
   end
 
   def test_binary_11010_is_decimal_26
-    skip
     assert_equal 26, Binary.to_decimal('11010')
   end
 
   def test_binary_10001101000_is_decimal_1128
-    skip
     assert_equal 1128, Binary.to_decimal('10001101000')
   end
 
   def test_binary_ignores_leading_zeros
-    skip
     assert_equal 31, Binary.to_decimal('000011111')
   end
 
