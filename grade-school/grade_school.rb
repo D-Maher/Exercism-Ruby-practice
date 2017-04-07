0245f21839674906c3ff3563a0a78b2aa79713d7
Danny Maher <dmaher724@gmail.com>
2017-04-07 13:20:34 -0500

    Declare class School, add instance variable student_roster, add instance method students to show the roster of students in a given grade

--- grade-school/grade_school.rb
+++ grade-school/grade_school.rb
@@ -1,4 +1,21 @@
+class School
 
+  def initialize
+    @student_roster = {}
+  end
+
+  def students(grade)
+    return [] if student_roster[grade].nil?
+
+    student_roster[grade]
+  end
+
+
+  private
+
+  attr_accessor :student_roster
+
+end
 
 
 module BookKeeping
