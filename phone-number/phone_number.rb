4ecb597d483236fff19e39441acc98f0daf05a2b
Danny Maher <dmaher724@gmail.com>
2017-04-10 15:24:00 -0500

    Adjust number method to handle invalid input

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -5,7 +5,19 @@ class PhoneNumber
   end
 
   def number
+    return "0000000000" unless phone.scan(/[a-zA-Z]/).empty?
+
     number = phone.scan(/\d*/).join
+
+    if number.length != 10
+      if number.length == 11 && number.chr == "1"
+        number = number[1..-1]
+      else
+        number = "0000000000"
+      end
+    end
+
+    number
   end
 
 
