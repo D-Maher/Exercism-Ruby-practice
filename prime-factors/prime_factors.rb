d22007448b00d700c5ecc5c6b2d0a2e71e41bc3a
Danny Maher <dmaher724@gmail.com>
2017-04-11 11:49:08 -0500

    Remove unnecessary is_prime? method, adjust for method to only increment the divisor when the input number % divisor is not equal to 0

--- prime-factors/prime_factors.rb
+++ prime-factors/prime_factors.rb
@@ -3,24 +3,18 @@ class PrimeFactors
   def self.for(num)
     prime_factors = []
 
-      for possible_factor in 2...num
-        if num % possible_factor == 0 && possible_factor.is_prime?
-          prime_factors << possible_factor
-        end
+    divisor = 2
+
+    while divisor <= num
+      if num % divisor == 0
+        prime_factors << divisor
+        num = num / divisor
+      else
+        divisor += 1
       end
+    end
 
     prime_factors
   end
-
-  def is_prime?(num)
-    return false if num < 2
-    return false if num.even?
-
-    for counter in 2...num
-      return false if num % counter == 0
-    end
-
-    true
-  end
-
+  
 end
