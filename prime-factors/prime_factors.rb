930addb94465ca6d4f49601dea50312cbcad0d50
Danny Maher <dmaher724@gmail.com>
2017-04-11 11:36:12 -0500

    Add method is_prime? to PrimeFactors class to determine whether or not a factor is prime

--- prime-factors/prime_factors.rb
+++ prime-factors/prime_factors.rb
@@ -4,4 +4,15 @@ class PrimeFactors
 
   end
 
+  def is_prime?(num)
+    return false if num < 2
+    return false if num.even?
+
+    for counter in 2...num
+      return false if num % counter == 0
+    end
+
+    true
+  end
+
 end
