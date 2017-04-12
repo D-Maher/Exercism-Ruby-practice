3155cec1f59bf113c237c23bcb23cee77ae4b77a
Danny Maher <dmaher724@gmail.com>
2017-04-12 12:27:57 -0500

    Add instance method to_decimal to covert trinary number to decimal number

--- trinary/trinary.rb
+++ trinary/trinary.rb
@@ -1,6 +1,28 @@
 class Trinary
 
+  def initialize(trinary_string)
+    @trinary_string = trinary_string
+  end
 
+  def to_decimal
+    trinary_split = trinary_string.chars.map(&:to_i)
+
+    exponent = trinary_split.length - 1
+
+    decimal = 0
+
+    trinary_split.each do |num|
+      decimal += num * (3 ** exponent)
+      exponent -= 1
+    end
+
+    decimal
+  end
+
+
+  private
+
+  attr_reader :trinary_string
 
 end
 
