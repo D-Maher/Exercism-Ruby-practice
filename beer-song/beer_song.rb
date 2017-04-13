487eb507ebc918f2374c867552a6358fb6fffc75
Danny Maher <dmaher724@gmail.com>
2017-04-13 13:47:17 -0500

    Declare class BeerSong, add instance method verse to return the verse of the song with a number of beers passed in

--- beer-song/beer_song.rb
+++ beer-song/beer_song.rb
@@ -1,5 +1,30 @@
+class BeerSong
 
+  def verse(num)
+    if num > 2
+      <<-TEXT
+#{num} bottles of beer on the wall, #{num} bottles of beer.
+Take one down and pass it around, #{num - 1} bottles of beer on the wall.
+TEXT
+    elsif num == 2
+      <<-TEXT
+#{num} bottles of beer on the wall, #{num} bottles of beer.
+Take one down and pass it around, #{num - 1} bottle of beer on the wall.
+TEXT
+    elsif num == 1
+      <<-TEXT
+#{num} bottle of beer on the wall, #{num} bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+TEXT
+    else
+      <<-TEXT
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+TEXT
+    end
+  end
 
+end
 
 
 module BookKeeping
