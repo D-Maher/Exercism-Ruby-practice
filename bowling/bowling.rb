26b24832608c8196b556a79fbcf18d5e52d13185
Danny Maher <dmaher724@gmail.com>
2017-04-14 13:42:31 -0500

    Declare class Game, add instance methods roll and score, adjust score method to calculate gutter ball games, games without spares or strikes, and games with spares in the first nine frames

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,4 +1,41 @@
+require 'pry'
 
+class Game
+
+  def initialize
+    @rolls = []
+  end
+
+
+  def roll(pins)
+    rolls << pins
+  end
+
+  def score
+    total_score = 0
+
+    frames = rolls.each_slice(2).to_a
+
+    frames.each do |frame|
+      frame_score = 0
+
+      if frame.reduce(:+) == 10
+        frame_score += 10 + frames[frames.index(frame) + 1][0]
+      else
+        frame_score = frame.reduce(:+)
+      end
+
+      total_score += frame_score
+    end
+
+    total_score
+  end
+
+  private
+
+  attr_reader :rolls
+
+end
 
 
 module BookKeeping
