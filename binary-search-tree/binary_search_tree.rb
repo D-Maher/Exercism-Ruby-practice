da0ab9d5229d32c0ba08f029e5c51f7d5e8e3e4e
Danny Maher <dmaher724@gmail.com>
2017-05-12 15:54:02 -0500

    Declare binary search tree class Bst, add method to insert new data into the binary search tree based on data value

--- binary-search-tree/binary_search_tree.rb
+++ binary-search-tree/binary_search_tree.rb
@@ -1,6 +1,21 @@
-
-
-
-module BookKeeping
+class Bst
   VERSION = 1
+
+  attr_reader :data
+  attr_accessor :left, :right
+
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+
+  def insert(new_data)
+    if new_data <= data
+      self.left = Bst.new(new_data)
+    else
+      self.right = Bst.new(new_data)
+    end
+  end
+
 end
