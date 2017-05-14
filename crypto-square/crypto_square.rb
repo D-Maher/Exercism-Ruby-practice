3558a7e499ec85b3efe5636a237d4263d6f44ebb
Danny Maher <dmaher724@gmail.com>
2017-05-14 15:23:40 -0500

    Write method to determine chunks of plaintext segments that comprise the crypto square

--- crypto-square/crypto_square.rb
+++ crypto-square/crypto_square.rb
@@ -10,8 +10,18 @@ class Crypto
 
   def size
     square_size = Math.sqrt(normalize_plaintext.length)
-
     square_size.to_i == square_size ? square_size : square_size.floor + 1
+  end
+
+  def plaintext_segments
+    square = []
+    normalized_string = normalize_plaintext
+
+    until normalized_string.length == 0
+      square << normalized_string.slice!(0...size)
+    end
+    
+    square
   end
 
 
