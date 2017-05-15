e00a37eea796c7092701f976d2ad9dd12f395db8
Danny Maher <dmaher724@gmail.com>
2017-05-14 21:38:03 -0500

    Adjust at method to roll over hours if they are greater than 24

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,6 +1,28 @@
 class Clock
 
   def self.at(hour, minute)
+    if hour >= 24
+      until hour < 24
+        hour -= 24
+      end
+    elsif hour < 0
+      until hour >= 0
+        hour += 24
+      end
+    end
+        
+    if minute > 59
+      until minute <= 59
+        minute -= 60
+        hour += 1
+      end
+    elsif minute < 0
+      until minute >= 0
+        minute += 60
+        hour -= 1
+      end
+    end
+        
     hour = hour.to_s
     minute = minute.to_s
 
