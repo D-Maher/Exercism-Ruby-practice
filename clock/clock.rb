681514ccccc99b4520133fc12306d911fc38b825
Danny Maher <dmaher724@gmail.com>
2017-05-14 21:34:47 -0500

    Declare class Clock, add class method at to return a time (in 24-hour format) based on an hour integer and minute integer passed in

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,3 +1,16 @@
+class Clock
+
+  def self.at(hour, minute)
+    hour = hour.to_s
+    minute = minute.to_s
+
+    hour.length == 1 ? hour = "0#{hour}" : hour.to_s
+    minute.length == 1 ? minute = "0#{minute}" : minute.to_s
+
+    "#{hour}:#{minute}"
+  end
+
+end
 
 
 module BookKeeping
