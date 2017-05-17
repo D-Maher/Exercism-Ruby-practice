cce48c247483f7ad4b3c1ea46af540601bce8bba
Danny Maher <dmaher724@gmail.com>
2017-05-17 13:14:40 -0500

    Adjust encode method to encode messages on with 2 rails

--- rail-fence-cipher/rail_fence_cipher.rb
+++ rail-fence-cipher/rail_fence_cipher.rb
@@ -5,7 +5,31 @@ class RailFenceCipher
     return '' if message.empty?
     return message if rails == 1
 
+    encoded_message = ""
 
+    split_message = message.chars
+
+    # for 2 rails
+      # starting with the first letter, add every other letter to encoded_message
+      # next, starting with the second letter, add every other letter to encoded_message
+
+    i = 0
+
+    while i < rails
+      split_message.each_with_index do |char, index|
+        if (index - i) % rails == 0
+          encoded_message += char
+        end
+      end
+
+      i += 1
+    end 
+
+    # for 3 rails
+      # starting with the first letter, add the letter at the index 4 greater than the previous letter to encoded_message
+
+
+    encoded_message
   end
 
 end
