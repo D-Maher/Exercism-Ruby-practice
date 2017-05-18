420efd0dd15b392ca3dff2dffc5826bfb7f23d16
Danny Maher <dmaher724@gmail.com>
2017-05-18 11:27:02 -0500

    Adjust decode method to decode encoded messages with any number of rails

--- rail-fence-cipher/rail_fence_cipher.rb
+++ rail-fence-cipher/rail_fence_cipher.rb
@@ -38,7 +38,58 @@ class RailFenceCipher
     return "" if message.empty?
     return message if number_of_rails == 1
 
-    
+    decoded_message = ""
+
+    fence = []
+    number_of_rails.times { fence << [] }
+
+    rail = 0
+    bottom_rail = number_of_rails - 1
+    going_down = true
+    message.length.times do
+      if rail == 0
+        fence[rail] << "?"
+        rail += 1
+        going_down = true
+      elsif rail < bottom_rail && going_down
+        fence[rail] << "?"
+        rail += 1
+      elsif rail < bottom_rail && !going_down
+        fence[rail] << "?"
+        rail -= 1
+      elsif rail == bottom_rail
+        fence[rail] << "?"
+        rail -= 1
+        going_down = false
+      end
+    end
+
+    start_index = 0
+    fence.each do |rail|
+      rail.replace(message.chars.slice(start_index, rail.length))
+      start_index += rail.length
+    end
+
+    rail = 0
+    message.length.times do
+      if rail == 0
+        decoded_message += fence[rail].shift
+        rail += 1
+        going_down = true
+      elsif rail < bottom_rail && going_down
+        decoded_message += fence[rail].shift
+        rail += 1
+      elsif rail < bottom_rail && !going_down
+        decoded_message += fence[rail].shift
+        rail -= 1
+      elsif rail == bottom_rail
+        decoded_message += fence[rail].shift
+        rail -= 1
+        going_down = false
+      end
+    end
+
+    decoded_message
   end
 
 end
--- rail-fence-cipher/rail_fence_cipher_test.rb
+++ rail-fence-cipher/rail_fence_cipher_test.rb
@@ -41,13 +41,11 @@ class RailFenceCipherTest < Minitest::Test
   end
 
   def test_decode_with_two_rails
-    skip
     assert_equal 'XOXOXOXOXOXOXOXOXO',
                  RailFenceCipher.decode('XXXXXXXXXOOOOOOOOO', 2)
   end
 
   def test_decode_with_three_rails
-    skip
     assert_equal 'THEDEVILISINTHEDETAILS',
                  RailFenceCipher.decode('TEITELHDVLSNHDTISEIIEA', 3)
   end
