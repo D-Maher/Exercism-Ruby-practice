65d173a8b61c6ebd62726a02946eedd592a428fb
Danny Maher <dmaher724@gmail.com>
2017-05-18 13:11:50 -0500

    Declare class Acronym, unlock all tests, write class method abbreviate to convert phrases into acronyms

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,4 +1,20 @@
+class Acronym
 
+  def self.abbreviate(phrase)
+    acronym = ""
+
+    phrase.sub!(/([a-z])([A-Z])/) { |s| s.insert(1, " ") } 
+
+    words = phrase.scan(/\w+/)
+
+    words.each do |word|
+      acronym += word[0].upcase
+    end
+
+    acronym
+  end
+
+end
 
 
 module BookKeeping
--- acronym/acronym_test.rb
+++ acronym/acronym_test.rb
@@ -5,37 +5,30 @@ require_relative 'acronym'
 # Common test data version: 1.0.0 dcfe476
 class AcronymTest < Minitest::Test
   def test_basic
-    # skip
     assert_equal "PNG", Acronym.abbreviate('Portable Network Graphics')
   end
 
   def test_lowercase_words
-    skip
     assert_equal "ROR", Acronym.abbreviate('Ruby on Rails')
   end
 
   def test_camelcase
-    skip
     assert_equal "HTML", Acronym.abbreviate('HyperText Markup Language')
   end
 
   def test_punctuation
-    skip
     assert_equal "FIFO", Acronym.abbreviate('First In, First Out')
   end
 
   def test_all_caps_words
-    skip
     assert_equal "PHP", Acronym.abbreviate('PHP: Hypertext Preprocessor')
   end
 
   def test_non_acronym_all_caps_word
-    skip
     assert_equal "GIMP", Acronym.abbreviate('GNU Image Manipulation Program')
   end
 
   def test_hyphenated
-    skip
     assert_equal "CMOS", Acronym.abbreviate('Complementary metal-oxide semiconductor')
   end
 
@@ -57,7 +50,6 @@ class AcronymTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
     assert_equal 3, BookKeeping::VERSION
   end
 end
