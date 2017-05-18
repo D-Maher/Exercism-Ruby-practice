a077fe10c1356f7a3c55475f965e6dee3ac0d336
Danny Maher <dmaher724@gmail.com>
2017-05-18 15:48:35 -0500

    Write class method from_dna to instantiate a new instance of Nucleotide from a DNA sequence passed in, write instance method count to return 0 when the DNA sequence is empty

--- nucleotide-count/nucleotide_count.rb
+++ nucleotide-count/nucleotide_count.rb
@@ -1,5 +1,16 @@
 class Nucleotide
+  attr_reader :dna_strand
 
+  def initialize(dna_strand)
+    @dna_strand = dna_strand
+  end
 
+  def self.from_dna(dna_strand)
+    new(dna_strand)
+  end
+
+  def count(nucleotide)
+    return 0 if dna_strand.empty?
+  end
 
 end
