61f18479870725473a4bddf9c57989c5d4f26fac
Danny Maher <dmaher724@gmail.com>
2017-05-23 13:03:08 -0500

    Add class methods to construct the song cumulatively

--- food-chain/food_chain.rb
+++ food-chain/food_chain.rb
@@ -13,6 +13,43 @@ class FoodChain
 
   private_constant :ANIMAL_LINES
 
+  def self.song
+    song = ""
+
+    all_animals = ANIMAL_LINES.keys
+
+    all_animals.each_with_index do |animal, index|
+      if index == 0
+        song += self.swallow(animal)
+      elsif index < all_animals.length - 1
+        song += self.swallow(animal)
+        i = index
+
+        until i == 0
+          song += self.swallow_and_catch(all_animals[i], all_animals[i - 1])
+          i -= 1
+        end
+
+        song += ANIMAL_LINES["fly"]
+      else
+        song += self.swallow(animal)
+      end
+      
+      song += "\n"
+    end
+
+    song
+  end
+
+  def self.swallow(animal)
+    "I know an old lady who swallowed a #{animal}.\n" + ANIMAL_LINES[animal]
+  end
+
+  def self.swallow_and_catch(predator, prey)
+    swallow_and_catch = "She swallowed the #{predator} to catch the #{prey}" 
+    prey == "spider" ? swallow_and_catch += " that wriggled and jiggled and tickled inside her.\n" : swallow_and_catch += ".\n"
+    swallow_and_catch
+  end
 
 end
 
