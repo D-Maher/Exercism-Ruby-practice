d31ea2e0736d6183c0644771fa913ea9b8faf18e
Danny Maher <dmaher724@gmail.com>
2017-05-23 13:22:52 -0500

    Create file saddle_points.rb, declare class Matrix, add convert_to_grid instance method to convert matrix string into nested array

---
+++ saddle-points/saddle_points.rb
@@ -0,0 +1,31 @@
+class Matrix
+
+  attr_reader :rows, :columns
+
+  def initialize(matrix_string)
+    @matrix_string = matrix_string
+    @rows = convert_to_grid
+  end
+
+  def convert_to_grid
+    grid = []
+
+    matrix_string.split("\n").each do |row_string|
+      row = []
+
+      row_string.split(" ").each do |cell|
+        row << cell.to_i
+      end
+
+      grid << row
+    end
+    
+    grid
+  end
+
+
+  private
+
+  attr_reader :matrix_string
+
+end
