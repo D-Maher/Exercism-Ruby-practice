fb5d3c5201823637093157dea062f9bee19478fd
Danny Maher <dmaher724@gmail.com>
2017-05-23 14:09:59 -0500

    Add instance methods to locate saddle points within a given matrix

--- saddle-points/saddle_points.rb
+++ saddle-points/saddle_points.rb
@@ -24,6 +24,38 @@ class Matrix
     grid
   end
 
+  def greatest_in_row?(value, row)
+    row.each do |other_value|
+      return false if other_value > value
+    end
+
+    true
+  end
+
+  def least_in_column?(value, column)
+    column.each do |other_value|
+      return false if other_value < value
+    end
+
+    true 
+  end
+
+  def saddle_points
+    saddle_points = []
+
+    (0...rows.length).each do |x|
+      (0...columns.length).each do |y|
+        value = rows[x][y]
+
+        if greatest_in_row?(value, rows[x]) && least_in_column?(value, columns[y])
+          saddle_points << [x, y]
+        end
+      end
+    end
+
+    saddle_points
+  end
+
 
   private
 
