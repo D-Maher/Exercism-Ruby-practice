2714c790efdef9b9f51cbf5c30f1516e37588ace
Danny Maher <dmaher724@gmail.com>
2017-05-23 12:58:44 -0500

    Declare class FoodChain, add constant containing unique lines for each animal in the song

--- food-chain/food_chain.rb
+++ food-chain/food_chain.rb
@@ -1,4 +1,20 @@
+class FoodChain
 
+  ANIMAL_LINES = {
+    "fly" => "I don't know why she swallowed the fly. Perhaps she'll die.\n",
+    "spider" => "It wriggled and jiggled and tickled inside her.\n",
+    "bird" => "How absurd to swallow a bird!\n",
+    "cat" => "Imagine that, to swallow a cat!\n",
+    "dog" => "What a hog, to swallow a dog!\n",
+    "goat" => "Just opened her throat and swallowed a goat!\n",
+    "cow" => "I don't know how she swallowed a cow!\n",
+    "horse" => "She's dead, of course!"
+  }
+
+  private_constant :ANIMAL_LINES
+
+
+end
 
 
 module BookKeeping
