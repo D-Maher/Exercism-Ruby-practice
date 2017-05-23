b0f5f2485c4fc5e273009d663b656ccd16391446
Danny Maher <dmaher724@gmail.com>
2017-05-23 13:16:03 -0500

    Add instance method to convert matrix string to a nested array, declare instance variables rows and columns based on that nested array

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,7 +1,30 @@
 class Matrix
 
+  attr_reader :rows, :columns
+
   def initialize(maxtrix_string)
     @maxtrix_string = maxtrix_string
+    @rows = convert_to_grid
+    @columns = rows.transpose
   end
+
+  def convert_to_grid
+    grid = []
+
+    maxtrix_string.split("\n").each do |row_string|
+      row = []
+      row_string.split(" ").each do |cell|
+        row << cell.to_i
+      end
+      grid << row
+    end
+
+    grid
+  end
+
+
+  private 
+
+  attr_reader :maxtrix_string
 
 end
